c512199dea302bd3804757ba5843f36bc0769e43
José Luis Becerra Sainz <130393681+PpSainz12@users.noreply.github.com>
2023-11-12 14:10:38 -0600

    otros cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//componentes
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+//paginas
 import { LoginComponent } from '../pages/auth/login/login.component';
 import { RecoverComponent } from '../pages/auth/recover/recover.component';
 import { RegisterComponent } from '../pages/auth/register/register.component';
 import { HomeComponent } from '../pages/home/home.component';
-import { AssemblerComponent } from './assembler/assembler.component';
+import { AssemblerComponent } from '../pages/assembler/assembler.component';
 
 
 @NgModule({
