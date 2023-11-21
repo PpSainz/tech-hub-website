db22e08935892aef9294cfd7a218798ba85ae2a4
José Luis Becerra Sainz <130393681+PpSainz12@users.noreply.github.com>
2023-11-21 10:26:49 -0600

    otros cambios

--- src/pages/assembler/noob/noob.component.ts
+++ src/pages/assembler/noob/noob.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ComputersService } from 'src/app/services/computers.service';
 
 @Component({
   selector: 'app-noob',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./noob.component.scss']
 })
 export class NoobComponent {
+  recomendation_select: any = {
+    Tag: '',
+  }
+  recomendation_list: any = [];
 
+  constructor(
+    private ComputersService: ComputersService,
+  ) { this.getlist(); }
+
+  async getlist() {
+    this.ComputersService.getPCS({}).subscribe(async(return_list:any)=>{
+      this.recomendation_list = return_list;
+    });
+  }
+
+  async selectPC() {
+    this.recomendation_select = this.recomendation_list.filter((pc: any) => pc.Tag == this.recomendation_select.Tag);
+  }
 }
