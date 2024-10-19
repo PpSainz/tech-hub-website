d6468d31ff040d2c0df0e91e1ca241547c5324b6
Yahir Leo <123343914+Yiro13@users.noreply.github.com>
2024-10-19 11:56:41 -0600

    Update app.config.ts
    changed api url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,6 @@ export class AppConfig {
     static production: boolean               = true;
     static readonly app_version: string      = '0.0.0';
     static readonly api_local: string        = 'http://localhost:3000/';
-    static readonly api_production: string   = 'https://tech-hub-2245.onrender.com/';
-    static api_url: string                   = 'https://tech-hub-2245.onrender.com/';
+    static readonly api_production: string   = 'https://tech-hub-backend.onrender.com/';
+    static api_url: string                   = 'https://tech-hub-backend.onrender.com/';
 }
